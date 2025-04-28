e94813e8511a18e05f967401af67eb91ae2d56ec
cellbesmanos <cell.besmanos0119@gmail.com>
2025-04-28 20:41:12 +0800

    Build hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,21 @@ export default function Hero() {
   return (
     <header className="hero">
       <Navbar />
+
+      <section>
+        <h1>Transforming ideas into Impactful software</h1>
+
+        <div className="hero__card">
+          <div className="hero__card-img">
+            <img src="/public/profile-picture.png" alt="Profile picture of Christian" />
+          </div>
+
+          <div className="hero__card-description">
+            <h2>Christian Lhie Besmanos</h2>
+            <p>Front-End Developer</p>
+          </div>
+        </div>
+      </section>
     </header>
   )
 }
