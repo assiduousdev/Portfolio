858a2efdb8b562a3b4f3bbb58438c37fc3f4855e
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2025-04-29 19:30:51 +0800

    Animate hero card

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,16 +5,18 @@ import Navbar from "../Navbar/Navbar";
 import "./Hero.css";
 
 export default function Hero() {
-  const MAX_ANIMATION_LENGTH_IN_MILLISECONDS = 400;
+  const MAX_ANIMATION_LENGTH_IN_MILLISECONDS = 600;
 
   const heading = "Transforming ideas into Impactful software";
   const characters = heading.split("");
+
+  const animationDelays = MAX_ANIMATION_LENGTH_IN_MILLISECONDS / characters.length; 
 
   function getHeadingMarkup() {
     let formatted = "";
     for (let i = 0; i < characters.length; i++) {
       const char = characters[i];
-      const delay = MAX_ANIMATION_LENGTH_IN_MILLISECONDS / characters.length * i;
+      const delay = animationDelays * i;
 
       if (i === 0) {
         formatted += "<span class=\"word\">" + buildCharacterTemplate(delay, char);
@@ -27,7 +29,6 @@ export default function Hero() {
       }
     }
 
-    console.log(formatted);
     return { __html: formatted };
   }
 
@@ -39,17 +40,30 @@ export default function Hero() {
     <header className="hero">
       <Navbar />
 
-      <section>
-        <h1 style={{ "--length":  `${MAX_ANIMATION_LENGTH_IN_MILLISECONDS}ms`}} dangerouslySetInnerHTML={getHeadingMarkup()}></h1>
+      <section 
+        style={{ "--length":  `${MAX_ANIMATION_LENGTH_IN_MILLISECONDS}ms`}}
+      >
+        <h1 dangerouslySetInnerHTML={getHeadingMarkup()}></h1>
 
-        <div className="hero__card">
-          <div className="hero__card-img">
+        <div className="hero__card" >
+          <div 
+            className="hero__card-img"
+            style={{ "--delay": `${ animationDelays * (characters.length + 1) }ms` }}
+          >
             <img src="/profile-picture.png" alt="Profile picture of Christian" />
           </div>
 
           <div className="hero__card-description">
-            <h2>Christian Lhie Besmanos</h2>
-            <p>Front-End Developer</p>
+            <h2 
+              style={{ "--delay": `${ animationDelays * (characters.length + 2) }ms` }}
+            >
+              Christian Lhie Besmanos
+            </h2>
+            <p 
+              style={{ "--delay": `${ animationDelays * (characters.length + 3) }ms` }}
+            >
+              Front-End Developer
+            </p>
           </div>
         </div>
       </section>
