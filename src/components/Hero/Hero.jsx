d2deea8233641d472d459b3d8eae86664c6eec1e
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2025-04-29 18:22:25 +0800

    Animate hero heading

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import Navbar from "../Navbar/Navbar";
 
 import "./Hero.css";
 
 export default function Hero() {
+  const MAX_ANIMATION_LENGTH_IN_MILLISECONDS = 400;
+
+  const heading = "Transforming ideas into Impactful software";
+  const characters = heading.split("");
+
+  function getHeadingMarkup() {
+    let formatted = "";
+    for (let i = 0; i < characters.length; i++) {
+      const char = characters[i];
+      const delay = MAX_ANIMATION_LENGTH_IN_MILLISECONDS / characters.length * i;
+
+      if (i === 0) {
+        formatted += "<span class=\"word\">" + buildCharacterTemplate(delay, char);
+      } else if (i === characters.length - 1) {
+        formatted += buildCharacterTemplate(delay, char) + "</span>";
+      } else if (char === " ") {
+        formatted += "</span> <span class=\"word\">"
+      } else {
+        formatted += buildCharacterTemplate(delay, char);
+      }
+    }
+
+    console.log(formatted);
+    return { __html: formatted };
+  }
+
+  function buildCharacterTemplate(delay, char) {
+    return `<span class="char animate" style="--delay: ${delay}ms">${char}</span>`;
+  }
+
   return (
     <header className="hero">
       <Navbar />
 
       <section>
-        <h1>Transforming ideas into Impactful software</h1>
+        <h1 style={{ "--length":  `${MAX_ANIMATION_LENGTH_IN_MILLISECONDS}ms`}} dangerouslySetInnerHTML={getHeadingMarkup()}></h1>
 
         <div className="hero__card">
           <div className="hero__card-img">
